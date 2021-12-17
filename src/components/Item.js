a84fdfede2ab92098628e469363604cc135f3dd5
Indie Mike <indiemike01@gmail.com>
2021-12-17 19:36:52 +0200

    Show more feature

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { tours } from "../data";
 
 const Item = () => {
+  const [description, setDescription] = useState([
+    tours.map((tour) => {
+      return {
+        id: tour.id,
+        description: tour.description1,
+        text: "Show more",
+      };
+    }),
+  ]);
+  // const [text, setText] = useState("Show more");
+
+  const [displayDescription2, setDisplayDescription2] = useState(false);
+  const [showMore, setShowMore] = useState("Show more");
+  const displayMoreDescription = () => {
+    setDisplayDescription2(!displayDescription2);
+    setShowMore(displayDescription2 ? "Show  more" : "Show less");
+  };
+
+  console.log(description);
   return (
     <>
       {tours.map((tour) => {
         return (
-          <div>
+          <div key={tour.name}>
             <img src={tour.photo} alt="" />
-            <div>
-              <h4>{tour.name}</h4>
+            <div key={tour.id}>
+              <h4></h4>
               <h4>{tour.price}</h4>
               <h4>
                 {tour.description1}
-                {tour.description2}
+                {displayDescription2 && tour.description2}
+                <button onClick={() => displayMoreDescription()}>
+                  {showMore}
+                </button>
               </h4>
               <button type="btn">Not interested</button>
               <br />
